a0417d3701ca58de30f74e4d5983b647523947a4
Amitav Mitra <amitav.mitra@cern.ch>
2021-06-14 10:44:25 -0400

    forgot to define destructor

--- include/BUTextIO/BUTextIO.hh
+++ include/BUTextIO/BUTextIO.hh
@@ -6,7 +6,7 @@
 class BUTextIO {
 public:
     BUTextIO();
-    virtual ~BUTextIO();
+    virtual ~BUTextIO() {};
     virtual void AddOutputStream(Level::level level, std::ostream *os);
     virtual void ResetStreams(Level::level level);
     virtual void Print(Level::level level, const char *fmt, ...);
