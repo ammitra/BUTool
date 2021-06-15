167cb7494c52b4532c4f4a5dfc169ff1fac8394a
Dan Gastler <dgastler@gmail.com>
2021-06-15 12:34:41 -0400

    Fixed a find bug

--- env.sh
+++ env.sh
@@ -3,7 +3,8 @@ export PATH=$PWD/bin/tool/:$PATH
 
 #setup LD_LIBRARY_PATHs for BUTool and any plugins
 #must be source AFTER build finishes
-plugin_paths=$(find $PWD/plugins/*/ -type d | xargs)
+#plugin_paths=$(find $PWD/plugins/* -type d | xargs)
+plugin_paths=$(ls -1 -d $PWD/plugins/*/ | xargs)
 export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 for path in $plugin_paths
 do
