c7bcafe816ecce1e59032ca1b5761f7d2837715b
Dan Gastler <Dan Gastler>
2021-06-15 12:08:43 -0400

    More Ubutnu fixes

--- env.sh
+++ env.sh
@@ -3,10 +3,10 @@ export PATH=$PWD/bin/tool/:$PATH
 
 #setup LD_LIBRARY_PATHs for BUTool and any plugins
 #must be source AFTER build finishes
-plugin_paths=$(find $PWD/plugins/*/lib -type d | xargs)
+plugin_paths=$(find $PWD/plugins/*/ -type d | xargs)
 export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 for path in $plugin_paths
 do
-    export LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=$path/lib:$LD_LIBRARY_PATH
 done
   
