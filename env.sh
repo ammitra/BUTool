781cb5778d35738140d61f99a00d63476f54e036
Dan Gastler <dgastler@gmail.com>
2021-06-15 09:59:45 -0400

    Switched ordering of env variable additions

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #set BUTool.exe in path
-export PATH+=:$PWD/bin/tool/
+export PATH=$PWD/bin/tool/:$PATH
 
 #setup LD_LIBRARY_PATHs for BUTool and any plugins
 #must be source AFTER build finishes
 plugin_paths=$(find $PWD/plugins/*/lib -type d | xargs)
-export LD_LIBRARY_PATH+=:$PWD/lib
+export LD_LIBRARY_PATH=$PWD/lib:$LD_LIBRARY_PATH
 for path in $plugin_paths
 do
-    export LD_LIBRARY_PATH+=:$path
+    export LD_LIBRARY_PATH=$path:$LD_LIBRARY_PATH
 done
   
